daa0bd1a282768833fb9cca6efdfa7f6041757b9
Admin <bmbmuhiddinova@gmail.com>
2021-03-09 16:05:00 +0500

    09.03.21. MediaFragment.kt

--- app/src/main/java/com/example/prayertime/ui/compass/CompassFragment.kt
+++ app/src/main/java/com/example/prayertime/ui/compass/CompassFragment.kt
@@ -19,6 +19,8 @@ class CompassFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
+
+
     ): View? {
         return inflater.inflate(R.layout.compass_fragment, container, false)
     }
